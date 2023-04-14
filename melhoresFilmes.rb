951654de6362d2c1888e7e24db16d6af02892ec1
JoaoGuilhermeMA <joomoreira36@gmail.com>
2023-04-14 00:56:59 -0300

    salvando o arquivo como csv

--- melhoresFilmes.rb
+++ melhoresFilmes.rb
@@ -4,11 +4,12 @@ require 'open-uri'
 doc = Nokogiri::HTML(URI.open('https://www.imdb.com/chart/top/?ref_=nv_mv_250'))
 
 table = doc.css('table.chart').css('tbody.lister-list')
+open('filmes.csv', 'w') do |arquivo|
+  table.search('tr').each do |tr|
+    title = tr.css('td.titleColumn').css('a').text
+    note = tr.css('td.ratingColumn').css('strong').text
 
-table.search('tr').each do |tr|
-  title = tr.css('td.titleColumn').css('a').text
-  note = tr.css('td.ratingColumn').css('strong').text
-
-  puts title + ' ' + note
-  next if tr['class'] == 'watchlistColumn'
+    arquivo.puts %("#{title.chomp}";"#{note.chomp}")
+    next if tr['class'] == 'watchlistColumn'
+  end
 end
